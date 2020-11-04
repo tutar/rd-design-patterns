e39a1d66973de39fbd1bb270866adb1d69810398
tutarbe <hiyunzhen@126.com>
2020-11-04 23:34:10 +0800

    init

---
+++ chain-responsibility/src/main/java/org/tutar/pattern/chain/ChainApplication.java
@@ -0,0 +1,15 @@
+package org.tutar.pattern.chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tutar
+ */
+@SpringBootApplication
+public class ChainApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ChainApplication.class,args);
+    }
+}
